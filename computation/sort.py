0afa47dac13476cdf0d79df8e039ab887fce4810
iKrz <krzywda.igor@protonmail.com>
2020-08-20 22:53:19 +0200

    two stages of cleaning noise added

--- computation/sort.py
+++ computation/sort.py
@@ -21,25 +21,64 @@ def quick_sort(arr_m, arr_s, low, high):
 		quick_sort(arr_m, arr_s, low, pi - 1)
 		quick_sort(arr_m, arr_s, pi + 1, high)
 
-def generate_filepath(filepath):
-	out = filepath[0:len(filepath) - 4]
-	out +="_OUT.CSV"
-	return out
-
 def sort_results(filepath):
 	distance = list()
 	load = list()
+	distance_clean = list()
+	load_clean = list()
+	distance_super_clean = list()
+	load_super_clean = list()
 	with open(filepath, newline = '\n') as f:
-		reader = csv.reader(f, delimiter=',')
+		reader = csv.reader(f, delimiter = ',')
 		for row in reader:
 			distance.append(float(row[0]))
 			load.append(float(row[1]))
 	quick_sort(distance,load,0,len(distance) - 1)
-	with open(generate_filepath(filepath), "w") as f:
-		csv_write = csv.writer(f, delimiter=',')
+	filepath_raw = filepath[0:len(filepath) - 4] + "_OUT_RAW.CSV"
+	with open(filepath_raw, "w") as f:
+		csv_write = csv.writer(f, delimiter = ',')
 		csv_write.writerow(['d'] + ['F'])
 		for i in range(0,len(load)):
 			csv_write.writerow([distance[i]] + [load[i]])
+	clean_output(distance, load, distance_clean, load_clean)
+	filepath_clean = filepath[0:len(filepath) - 4] + "_OUT_CLEAN.CSV"
+	with open(filepath_clean, "w") as f:
+		csv_write = csv.writer(f, delimiter = ',')
+		csv_write.writerow(['d'] + ['F'])
+		for i in range(0, len(distance_clean)):
+			csv_write.writerow([distance_clean[i]] + [load_clean[i]])
+	super_clean_output(distance_clean, load_clean, distance_super_clean, load_super_clean)
+	filepath_super_clean = filepath[0:len(filepath) - 4] + "_OUT_CLEAN_SUPER.CSV"
+	with open(filepath_super_clean, "w") as f:
+		csv_write = csv.writer(f, delimiter = ',')
+		csv_write.writerow(['d'] + ['F'])
+		for i in range(0, len(distance_super_clean)):
+			csv_write.writerow([distance_super_clean[i]] + [load_super_clean[i]])
+
+def clean_output(distance, load, distance_c, load_c):
+	base = distance[0]
+	load_sum = load[0]
+	prev_pos = 0
+	for i in range(1, len(distance)):
+		if distance[i] != base:
+			mean = load_sum / (i - prev_pos) 
+			distance_c.append(distance[prev_pos])
+			load_c.append(mean)
+			base = distance[i]
+			load_sum = load[i]
+			prev_pos = i
+		else:
+			load_sum += load[i]
+
+def super_clean_output(distance, load, distance_c, load_c):
+	for i in range(0, len(distance)):
+		if load[i] > 1:
+			distance_c.append(distance[i])
+			load_c.append(load[i])
+
+def print_contents(list1, list2):
+	for i in range(0, len(list1)):
+		print(list1[i], "\t|\t", list2[i], "\n")
 
 if __name__ == "__main__":
 	for arg in sys.argv[1:]:
