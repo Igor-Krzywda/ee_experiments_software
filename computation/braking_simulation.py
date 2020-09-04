f3031b8b171fa6afe4098f4c97f497538ee790aa
iKrz <krzywda.igor@protonmail.com>
2020-09-04 13:06:04 +0200

    simulation working, minor bugs

--- computation/braking_simulation.py
+++ computation/braking_simulation.py
@@ -81,6 +81,9 @@ class Simulation(Bicycle):
 		self.v = v
 		self.incline = incline
 		self.filepath = filepath
+		self.s = 0
+		self.t = 0
+		self.work_dir = ''
 
 	def load_front(self, bf):
 		return (self.weight * self.d + bf * self.h) / self.l
@@ -92,9 +95,7 @@ class Simulation(Bicycle):
 		bf = self.bff + self.bfb
 		max_braking = self.weight * self.u
 		if self.load_rear(bf) < 0:
-			print(bf, self.load_rear(bf), self.load_front(bf))
-			bf -= 1		# optimize 
-		print(bf, self.load_rear(bf))
+			bf = ((self.weight / self.l) - self.weight * self.d) / self.h
 		return bf
 
 	def generate_directory(self):
@@ -106,27 +107,34 @@ class Simulation(Bicycle):
 				for i in dirs:
 					if i == directory:
 						dir_count += 1
-		path = os.path.join(self.filepath, directory)
-		os.mkdir(path)
-		path = os.path.join(self.filepath, directory, 'info')
+		self.work_dir = os.path.join(self.filepath, directory)
+		os.mkdir(self.work_dir)
+		path = os.path.join(self.work_dir, 'info')
 		os.mkdir(path)
 
 		with open(path + '/params.txt', 'w') as f:
 			f.write(params)
+
+		with open(path + '/conspect.csv', 'w') as f:
+			f.write('d,t,s\n')
 		
-		path = os.path.join(self.filepath, directory, 'data')
+		path = os.path.join(self.work_dir, 'data')
 		os.mkdir(path)
 
 		print('directory ', path, 'created')
-		return path
+
+	def conspect(self):
+		with open(self.work_dir + '/info/conspect.csv', 'a') as f:
+			csv_wr = csv.writer(f, delimiter = ',')
+			csv_wr.writerow([str("{0:.2f}".format(self.d)) + '.csv'] + ["{0:.2f}".format(self.t)] + ["{0:.2f}".format(self.s)])
 
 	def generate_data(self):
-		data_path = self.generate_directory()
+		self.generate_directory()
 		v = self.v
-		self.a = 5
-		t = ff = fr = fn = 0
-		while self.d < self.l:
-			path = os.path.join(data_path, str('{0:.2f}'.format(self.d)) + '.csv')
+		self.a = 0
+		ff = fr = fn = 0
+		while self.d < self.l - 0.2:
+			path = os.path.join(self.work_dir, 'data', str('{0:.2f}'.format(self.d)) + '.csv')
 			with open(path, 'w') as f:
 				csv_wr = csv.writer(f, delimiter = ',')
 				csv_wr.writerow(['t'] + ['a'] + ['v'] + ['s'] + ['F_n'] + ['F_f'] +['F_r'])
@@ -136,16 +144,21 @@ class Simulation(Bicycle):
 					fn = self.max_braking_force_net()
 					self.a = fn / self.m
 					v -= self.a * 0.1
-					s = abs(0.5 * self.a * 0.01)
-					t += 0.1
-					# ADDITIONAL LOGIC FOR REAR WHEEL LIFT-OFF
-					# ONE STEP AHEAD -> ITERATION -> GETTING BEST RESULT -> C++?
-					csv_wr.writerow(["{0:.2f}".format(t)]+["{0:.2f}".format(self.a)]+["{0:.2f}".format(v)]+["{0:.2f}".format(s)]+["{0:.2f}".format(fn)]+["{0:.2f}".format(ff)]+["{0:.2f}".format(fr)])
+					self.s += v * 0.1 + abs(0.5 * self.a * 0.01)
+					self.t += 0.1
+					csv_wr.writerow(["{0:.2f}".format(self.t)]+["{0:.2f}".format(self.a)]+["{0:.2f}".format(v)]+["{0:.2f}".format(self.s)]+["{0:.2f}".format(fn)]+["{0:.2f}".format(ff)]+["{0:.2f}".format(fr)])
+			self.conspect()
 			self.d += 0.01
-			t = 0
-			self.a = 0
 			v = self.v
-	
+			self.a = self.t = self.s = 0
+
+	class Data_processing:
+		def __init__(work_dir):
+			self.work_dir = work_dir
+
+		def plot_conspect(self):
+			for subdir, dirs, files in os.walk(self.filepath):
+
 if __name__ == "__main__":
-	sim = Simulation(1.1, 0.8, 0.36, 69.9, 62.3, 0.09, 0.08, 0.8, 85, 50, 0, '/home/ikrz/extended_essay/simulations/')
+	sim = Simulation(1.1, 0.8, 0.36, 69.9, 62.3, 0.09, 0.08, 0.8, 85, 10, 0, '/home/ikrz/extended_essay/simulations/')
 	sim.generate_data()
